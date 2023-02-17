3d7b0f19d0ba289ad679e1c59b6d2aa130aed0fc
Aarshi-GitHub <101852322+Aarshi-GitHub@users.noreply.github.com>
2023-02-17 15:07:55 +0530

    Linearize a Generic Tree O(N)

---
+++ Generic Trees/8. LinearizeTreeEff.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class GenericTree {
+    private static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<Node>();
+    }
+    
+    public static void levelOrderTraversal(Node node){
+        Queue<Node> q = new ArrayDeque<Node>();
+        q.add(node);
+        int level = q.size();
+        while(q.size()>0){
+            Node temp = q.remove();
+            System.out.print(temp.data+" ");
+            for(Node child : temp.children)
+                q.add(child);
+            level--;
+            if(level==0){
+                level = q.size();
+                System.out.println(".");
+            }
+        }
+    }
+    
+   public static Node linearizeEff(Node node){
+       if(node.children.size()==0) return node;
+       Node lt = linearizeEff(node.children.get(node.children.size()-1));
+       while(node.children.size()>1){
+           Node ln = node.children.remove(node.children.size()-1);
+           Node sln = linearizeEff(node.children.get(node.children.size()-1));
+           sln.children.add(ln);
+       }
+       return lt;
+   }
+    
+    public static void main(String[] args){
+        int arr[] = {10,20,-1,30,50,-1,60,-1,-1,40,-1,-1};
+        Node root = null;
+        Stack<Node> s = new Stack<Node>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==-1) s.pop();
+            else{
+                Node temp = new Node();
+                temp.data = arr[i];
+                if(s.isEmpty()) root = temp;
+                else s.peek().children.add(temp);
+                s.push(temp);
+            }
+        }
+        levelOrderTraversal(root);
+        linearizeEff(root);
+        System.out.println("Linearize Generic Tree : ");
+        levelOrderTraversal(root);
+    }
+}
