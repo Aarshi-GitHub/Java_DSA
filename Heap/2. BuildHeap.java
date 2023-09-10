82898f49fb321d8fd8ea3fb41e86a9d093ce66ac
Aarshi Aggarwal <101852322+Aarshi-GitHub@users.noreply.github.com>
2023-09-10 11:47:00 +0530

    Building a heap using heapify operation

---
+++ Heap/2. BuildHeap.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Practise {
+    
+    public static void swap(int arr[],int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    
+    public static void heapify(int arr[],int n,int pos){
+        int l = 2*pos+1;
+        int r = 2*pos+2;
+        int largest = pos;
+        if(l<n && arr[largest]<arr[l])
+            largest = l;
+        if(r<n && arr[largest]<arr[r])
+            largest = r;
+        if(largest!=pos){
+            swap(arr,largest,pos);
+            heapify(arr,n,largest);
+        }
+    }
+    
+    public static void buildHeap(int arr[],int n){
+        for(int  i = n/2 - 1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int n = arr.length;
+        heapSort(arr,n);
+        for(int x : arr) System.out.print(x+" ");
+    }
+}
